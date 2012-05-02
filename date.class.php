b4ef64c3bc00aa1d63da993102482e113cac45ce
Adam Shannon <adam@ashannon.us>
2012-05-02 18:56:43 -0500

    Update the formatting a bit.

--- date.class.php
+++ date.class.php
@@ -1,26 +1,15 @@
 <?php
 /**
- * BPA 2010
- * Adam Shannon
- * 2010-03-21
- */
+* BPA 2010
+* Adam Shannon
+* 2010-03-21
+*/
 
 class Date {
-	
-	static public $format = 'H:i:s \o\n Y-m-d';
-	
-	
-	/**
-	 * parse(int timestamp)
-	 * This function will parse a unix timestamp and return the 
-	 * value in self::$format's format.
-	 *
-	 * @parm: int timestamp - The UNIX timestamp
-	 * @return mixed - The date in self::$format's format.
-	 */
-	static function parse($timestamp) {
-		
-		return @date(self::$format, $timestamp);
-		
-	}
+
+  static private $format = 'H:i:s \o\n Y-m-d';
+
+  static function parse($timestamp, $fmt = self::$format) {
+    return @date(self::$format, $timestamp);
+  }
 }
